947967bf190562808a48bd530e10720e265ce077
Chris LeBailly <chris.lebailly@gmail.com>
2018-01-29 11:23:19 -0800

    Added AWS host name to env var

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,3 @@
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 set -x GREP_OPTIONS '--exclude-dir=.git --exclude-dir=venv --exclude-dir=venv-rd --exclude-dir=venv-pypy'
+. /Users/clebailly/.dotfiles/config/fish/aws.fish # Avoid commmiting the AWS host name, added to file in gitignore
