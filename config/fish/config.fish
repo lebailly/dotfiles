385c6d2c9ed19a683d42b64f6891f929891bcecb
Chris LeBailly <chris.lebailly@gmail.com>
2018-05-21 14:24:06 -0700

    Updated to start venv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,6 @@
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 set -x GREP_OPTIONS '--exclude-dir=.git --exclude-dir=venv --exclude-dir=venv-rd --exclude-dir=venv-pypy'
 . /Users/clebailly/.dotfiles/config/fish/aws.fish # Avoid commmiting the AWS host name, added to file in gitignore
+cd /Users/clebailly/Documents
+. ./venv-rd/bin/activate.fish
+cd /Users/clebailly/Documents/sci
